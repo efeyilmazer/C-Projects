564144112dd3159d992dab1936806ef4d0854937
Efe Yılmazer <efekan1232@gmail.com>
2021-12-04 23:52:27 +0300

    square meter price / area calculation
    Just started to learn C today!

---
+++ C-exercise.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+//Karenin kenar uzunluklarından alanını hesaplama
+/*
+int main () {
+
+    float kenar,cevre,alan;
+    printf("Karenin kenar uzunluğunu giriniz:");
+    scanf("%f", &kenar);
+    cevre = 4 * kenar;
+    alan = kenar * kenar;
+    printf("Karenin çevresi = %f, Karenin alanı = %f",cevre,alan);
+
+    getchar();
+    getchar();
+}
+
+*/
+
+// Yarıçapı girilen dairenin çevresini hesaplama
+/*
+int main () {
+    const float PI=3.14;
+    float yaricap,cevre,alan;
+
+    printf ("Dairenin Yarıçapını giriniz");
+    scanf("%f",&yaricap);
+
+    cevre = 2*PI*yaricap;
+    alan = PI*yaricap*yaricap;
+
+    printf ("Dairenin çevresi = %f\n",cevre);
+    printf ("Dairenin alani = %f\n",alan);
+    
+    getchar ();
+    getchar ();
+}
+*/
+
+//Kısa ve uzun kenar uzunlukları ve Metrekare fiyatı girilen halı ücreti
+
+
+/*
+int main () {
+    float kisa_kenar, uzun_kenar, alan, metrekare_fiyati, toplam_fiyat;
+
+    printf("Kısa ve uzun kenarları giriniz:");
+    scanf("%f%f",&kisa_kenar,&uzun_kenar);
+    printf("Metrekare fiyatını giriniz:");
+    scanf("%f",&metrekare_fiyati);
+
+    alan = kisa_kenar * uzun_kenar;
+    toplam_fiyat = alan * metrekare_fiyati;
+
+    printf ("Ödenecek toplam ücret = %0.2f",toplam_fiyat);
+
+    getchar();
+    getchar();
+}
+
+*/
